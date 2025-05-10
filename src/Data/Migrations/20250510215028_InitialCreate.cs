649a00c3319c2737839616af7a7909b94ab02664
mhbrazuquinha <mhbrazuquinha@gmail.com>
2025-05-10 19:43:56 -0300

    Atualização para jogadores
    * adicionado CRUD para jogadores
    *ajuste no uso do banco de dados

---
+++ src/Data/Migrations/20250510215028_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace src.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Times",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    CidadeOrigem = table.Column<string>(type: "TEXT", nullable: true),
+                    DataCriacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EscudoUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    Titulos = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Times", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jogadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Idade = table.Column<int>(type: "INTEGER", nullable: false),
+                    TimeId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Posicao = table.Column<string>(type: "TEXT", nullable: true),
+                    NumeroCamisa = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogadores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jogadores_Times_TimeId",
+                        column: x => x.TimeId,
+                        principalTable: "Times",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogadores_TimeId",
+                table: "Jogadores",
+                column: "TimeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogadores");
+
+            migrationBuilder.DropTable(
+                name: "Times");
+        }
+    }
+}

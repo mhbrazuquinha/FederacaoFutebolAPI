4b33a0ca69776e24bc44737a94c5e859ffdeb489
Lixolasgasoso <nribeiro@cs.up.edu.br>
2025-05-10 16:56:03 -0300

    Adicionadas as configurações do Swagger

--- src/Models/Jogador.cs
+++ src/Models/Jogador.cs
@@ -1,4 +1,7 @@
-using System.ComponentModel.DataAnnotations; // Adicione esta linha no início do arquivo
+// Models/Jogador.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace FedercaoFutebolAPI.Models;
 
 public class Jogador
 {
@@ -7,10 +10,16 @@ public class Jogador
 
     [Required(ErrorMessage = "Nome é obrigatório")]
     [StringLength(100, MinimumLength = 3, ErrorMessage = "Nome deve ter entre 3 e 100 caracteres")]
-    public string Nome { get; set; }
+    public string Nome { get; set; } = string.Empty;
 
     [Range(15, 50, ErrorMessage = "Idade deve ser entre 15 e 50 anos")]
     public int Idade { get; set; }
 
-    // ... outras propriedades
+    // Relacionamento com Time
+    public int? TimeId { get; set; }
+    public Time? Time { get; set; }
+    
+    // Adicione mais propriedades conforme necessário
+    public string? Posicao { get; set; }
+    public int NumeroCamisa { get; set; }
 }
